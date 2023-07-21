73c392fbe052ff9a5cc192cc5a99aa888101a77b
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-21 17:41:46 +0200

    creo la mia tabella trains con i componenti

--- database/migrations/2023_07_21_145716_create_trains_table.php
+++ database/migrations/2023_07_21_145716_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("azienda", 60);
+            $table->string("stazione_di_partenza", 60);
+            $table->string("stazione_di_arrivo", 60);
+            $table->dateTime("orario_di_partenza");
+            $table->dateTime("orario_di_arrivo");
+            $table->smallInteger("codice_treno")->unsigned();
+            $table->tinyInteger("numero_carrozze");
+            $table->boolean("in_orario"); 
+            $table->boolean("cancellato");
             $table->timestamps();
         });
     }
